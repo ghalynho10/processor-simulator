b89a8a4a3b2803237d502b9b02069bace91c201f
ghalynho10 <mghalynho@gmail.com>
2021-04-25 21:52:28 -0400

    finish MSI

--- protocols/MESI_protocol.cpp
+++ protocols/MESI_protocol.cpp
@@ -8,77 +8,70 @@ extern Simulator *Sim;
 /*************************
  * Constructor/Destructor.
  *************************/
-MESI_protocol::MESI_protocol (Hash_table *my_table, Hash_entry *my_entry)
-    : Protocol (my_table, my_entry)
+MESI_protocol::MESI_protocol(Hash_table *my_table, Hash_entry *my_entry)
+    : Protocol(my_table, my_entry)
+{
+    this->state = MESI_CACHE_I;
+}
+
+MESI_protocol::~MESI_protocol()
 {
 }
 
-MESI_protocol::~MESI_protocol ()
-{    
+void MESI_protocol::dump(void)
+{
+    const char *block_states[5] = {"X", "I", "S", "E", "M"};
+    fprintf(stderr, "MESI_protocol - state: %s\n", block_states[state]);
 }
 
-void MESI_protocol::dump (void)
+void MESI_protocol::process_cache_request(Mreq *request)
 {
-    const char *block_states[5] = {"X","I","S","E","M"};
-    fprintf (stderr, "MESI_protocol - state: %s\n", block_states[state]);
-}
-
-void MESI_protocol::process_cache_request (Mreq *request)
-{
-	switch (state) {
+    switch (state)
+    {
 
     default:
-        fatal_error ("Invalid Cache State for MESI Protocol\n");
+        fatal_error("Invalid Cache State for MESI Protocol\n");
     }
 }
 
-void MESI_protocol::process_snoop_request (Mreq *request)
+void MESI_protocol::process_snoop_request(Mreq *request)
 {
-	switch (state) {
-
+    switch (state)
+    {
 
     default:
-    	fatal_error ("Invalid Cache State for MESI Protocol\n");
+        fatal_error("Invalid Cache State for MESI Protocol\n");
     }
 }
 
-inline void MESI_protocol::do_cache_I (Mreq *request)
+inline void MESI_protocol::do_cache_I(Mreq *request)
 {
-
 }
 
-inline void MESI_protocol::do_cache_S (Mreq *request)
+inline void MESI_protocol::do_cache_S(Mreq *request)
 {
-
 }
 
-inline void MESI_protocol::do_cache_E (Mreq *request)
+inline void MESI_protocol::do_cache_E(Mreq *request)
 {
-
 }
 
-inline void MESI_protocol::do_cache_M (Mreq *request)
+inline void MESI_protocol::do_cache_M(Mreq *request)
 {
-
 }
 
-inline void MESI_protocol::do_snoop_I (Mreq *request)
+inline void MESI_protocol::do_snoop_I(Mreq *request)
 {
-
 }
 
-inline void MESI_protocol::do_snoop_S (Mreq *request)
+inline void MESI_protocol::do_snoop_S(Mreq *request)
 {
-
 }
 
-inline void MESI_protocol::do_snoop_E (Mreq *request)
+inline void MESI_protocol::do_snoop_E(Mreq *request)
 {
-
 }
 
-inline void MESI_protocol::do_snoop_M (Mreq *request)
+inline void MESI_protocol::do_snoop_M(Mreq *request)
 {
-
 }
-
--- protocols/MSI_protocol.cpp
+++ protocols/MSI_protocol.cpp
@@ -2,6 +2,7 @@
 #include "../sim/mreq.h"
 #include "../sim/sim.h"
 #include "../sim/hash_table.h"
+using namespace std;
 
 extern Simulator *Sim;
 
@@ -31,11 +32,20 @@ void MSI_protocol::process_cache_request(Mreq *request)
     case MSI_CACHE_I:
         do_cache_I(request);
         break;
+    case MSI_CACHE_IM:
+        do_cache_IM(request);
+        break;
+    case MSI_CACHE_IS:
+        do_cache_IS(request);
+        break;
     case MSI_CACHE_M:
         do_cache_M(request);
         break;
     case MSI_CACHE_S:
         do_cache_S(request);
+        break;
+    case MSI_CACHE_SM:
+        do_cache_SM(request);
         break;
 
     default:
@@ -56,6 +66,15 @@ void MSI_protocol::process_snoop_request(Mreq *request)
     case MSI_CACHE_S:
         do_snoop_S(request);
         break;
+    case MSI_CACHE_SM:
+        do_snoop_SM(request);
+        break;
+    case MSI_CACHE_IM:
+        do_snoop_IM(request);
+        break;
+    case MSI_CACHE_IS:
+        do_snoop_IS(request);
+        break;
 
     default:
         fatal_error("Invalid Cache State for MSI Protocol\n");
@@ -68,34 +87,227 @@ inline void MSI_protocol::do_cache_I(Mreq *request)
     {
     case LOAD:
         send_GETS(request->addr);
-        state = MSI_CACHE_S;
+        state = MSI_CACHE_IS;
+        // Sim->cache_misses++;
 
-        Sim->cache_misses++;
+        break;
     case STORE:
         send_GETM(request->addr);
-
-        state = MSI_CACHE_M;
-
+        state = MSI_CACHE_IM;
         Sim->cache_misses++;
+
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO CACHE_I' Client: I state shouldn't see this message\n");
+    }
+}
+
+inline void MSI_protocol::do_cache_IS(Mreq *request)
+{
+    switch (request->msg)
+    {
+    case LOAD:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("Should only have one outstanding request per processor!");
+        break;
+    case STORE:
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO CACHE_IS' Client: I state shouldn't see this message\n");
     }
 }
 
 inline void MSI_protocol::do_cache_S(Mreq *request)
 {
+    switch (request->msg)
+    {
+    case LOAD:
+        send_DATA_to_proc(request->addr);
+        break;
+    case STORE:
+        send_GETM(request->addr);
+        state = MSI_CACHE_SM;
+        Sim->cache_accesses++;
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        cout << "MESSAGE!!!!!:  " << request->msg << endl;
+        fatal_error("'DO CACHE_S' Client: S state shouldn't see this message\n");
+    }
+}
+inline void MSI_protocol::do_cache_IM(Mreq *request)
+{
+    switch (request->msg)
+    {
+    case LOAD:
+    case STORE:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("Should only have one outstanding request per processor!");
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO CACHE_IM' Client: I state shouldn't see this message\n");
+    }
+}
+
+inline void MSI_protocol::do_cache_SM(Mreq *request)
+{
+    switch (request->msg)
+    {
+    case LOAD:
+    case STORE:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("Should only have one outstanding request per processor!");
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO CACHE_SM' Client: S state shouldn't see this message\n");
+    }
 }
 
 inline void MSI_protocol::do_cache_M(Mreq *request)
 {
+    switch (request->msg)
+    {
+    case LOAD:
+        send_GETS(request->addr);
+        // Sim->cache_accesses++;
+        break;
+    case STORE:
+        send_DATA_to_proc(request->addr);
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO CACHE_M' Client: M state shouldn't see this message\n");
+    }
 }
 
 inline void MSI_protocol::do_snoop_I(Mreq *request)
 {
+    switch (request->msg)
+    {
+    case GETS:
+    case GETM:
+    case DATA:
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO SNOOP_I' Client: I state shouldn't see this message\n");
+    }
 }
 
 inline void MSI_protocol::do_snoop_S(Mreq *request)
 {
+    switch (request->msg)
+    {
+    case GETS:
+        // set_shared_line();
+
+        // send_DATA_on_bus(request->addr, request->src_mid);
+        break;
+    case GETM:
+        state = MSI_CACHE_I;
+        break;
+    case DATA:
+        state = MSI_CACHE_I;
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO SNOOP_S' Client: S state shouldn't see this message\n");
+    }
+}
+
+inline void MSI_protocol::do_snoop_SM(Mreq *request)
+{
+    switch (request->msg)
+    {
+    case GETS:
+    case GETM:
+
+    case DATA:
+        send_DATA_to_proc(request->addr);
+        if (get_shared_line())
+        {
+            state = MSI_CACHE_M;
+        }
+        else
+        {
+            state = MSI_CACHE_S;
+        }
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO SNOOP_SM' Client: S state shouldn't see this message\n");
+    }
+}
+inline void MSI_protocol::do_snoop_IM(Mreq *request)
+{
+    switch (request->msg)
+    {
+    case GETS:
+    case GETM:
+
+    case DATA:
+        send_DATA_to_proc(request->addr);
+        if (get_shared_line())
+        {
+            state = MSI_CACHE_M;
+        }
+        else
+        {
+            state = MSI_CACHE_I;
+        }
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO SNOOP_IM' Client: I state shouldn't see this message\n");
+    }
+}
+
+inline void MSI_protocol::do_snoop_IS(Mreq *request)
+{
+    switch (request->msg)
+    {
+    case GETS:
+    case GETM:
+
+    case DATA:
+        send_DATA_to_proc(request->addr);
+        if (get_shared_line())
+        {
+            state = MSI_CACHE_S;
+        }
+        else
+        {
+            state = MSI_CACHE_I;
+        }
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO SNOOP_IS' Client: I state shouldn't see this message\n");
+    }
 }
 
 inline void MSI_protocol::do_snoop_M(Mreq *request)
 {
+    switch (request->msg)
+    {
+    case GETS:
+        // send_DATA_on_bus(request->addr, request->src_mid);
+        state = MSI_CACHE_S;
+        break;
+    case GETM:
+        set_shared_line();
+        send_DATA_on_bus(request->addr, request->src_mid);
+        state = MSI_CACHE_I;
+        break;
+    case DATA:
+        fatal_error("'DATA WHYYYY' Should not see data for this line!  I have the line!");
+        break;
+    default:
+        request->print_msg(my_table->moduleID, "ERROR");
+        fatal_error("'DO SNOOP_M' Client: M state shouldn't see this message\n");
+    }
 }
